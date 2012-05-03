00b311060dd3663c32c49d118025526751c8a5b8
Christian Tellnes <christian@tellnes.no>
2012-05-03 21:59:58 +0200

    add proxy example with cache

---
+++ examples/proxy.js
@@ -0,0 +1,23 @@
+var dhfr = require('..')
+  , httpProxy = require('http-proxy')
+  ;
+
+
+httpProxy.createServer(
+  function(req, res, next) {
+    req.buffer = httpProxy.buffer(req);
+    next();
+  },
+  dhfr.middleware({
+    cache: {
+      defaultTTL: 10
+    }
+  }),
+  function(req, res, proxy) {
+    proxy.proxyRequest(req, res, {
+      host: 'localhost',
+      port: 80,
+      buffer: req.buffer
+    });
+  }
+).listen(1337);
