b2ba97558704b60a3d5ff608617e4608b4934497
Christian Tellnes <christian@tellnes.no>
2012-05-03 02:00:48 +0200

    add lookup example

---
+++ examples/lookup.js
@@ -0,0 +1,11 @@
+var dhfr = require('..');
+
+dhfr.lookup('example.com', function(err, result) {
+  if (err) throw err;
+
+  if (!result) {
+    console.log('No match');
+  } else {
+    console.log('Location': result.location);
+  }
+});
